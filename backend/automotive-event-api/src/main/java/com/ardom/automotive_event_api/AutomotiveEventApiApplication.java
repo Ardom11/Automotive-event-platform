c15f74d61a77dc96e59c716a4210fd450831e938
Andrian Petrykiv <andrik2007.com@gmail.com>
2026-05-25 11:12:49 +0200

    initial project structure

---
+++ backend/automotive-event-api/src/main/java/com/ardom/automotive_event_api/AutomotiveEventApiApplication.java
@@ -0,0 +1,13 @@
+package com.ardom.automotive_event_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveEventApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveEventApiApplication.class, args);
+	}
+
+}
---
+++ backend/automotive-event-api/src/test/java/com/ardom/automotive_event_api/AutomotiveEventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardom.automotive_event_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotiveEventApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
