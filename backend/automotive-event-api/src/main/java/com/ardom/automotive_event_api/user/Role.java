1295ee82dc442ccd6239d9d1ebc4ec2678883663
Andrian Petrykiv <andrik2007.com@gmail.com>
2026-05-26 21:49:31 +0200

    feat: create user entity

---
+++ backend/automotive-event-api/src/main/java/com/ardom/automotive_event_api/user/Role.java
@@ -0,0 +1,6 @@
+package com.ardom.automotive_event_api.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ backend/automotive-event-api/src/main/java/com/ardom/automotive_event_api/user/User.java
@@ -0,0 +1,71 @@
+package com.ardom.automotive_event_api.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name="google_id")
+    private String googleId;
+
+    @Column(name="created_at", updatable = false, nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name="modified_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+}
