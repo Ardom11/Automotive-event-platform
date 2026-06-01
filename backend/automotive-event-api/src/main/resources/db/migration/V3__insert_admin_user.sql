446b5b52a3c7eeb6b0f6dbcd64ffaee556b287de
Andrian Petrykiv <andrik2007.com@gmail.com>
2026-06-01 15:36:36 +0200

    feat(db): add admin insertion migration

---
+++ backend/automotive-event-api/src/main/resources/db/migration/V3__insert_admin_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, surname, email, password, role, created_at, modified_at)
+VALUES ('Admin',
+        'Admin',
+        'admin@automotive-event.com',
+        '$2a$12$mFoE.lIx3lEMMh2c93unqO/bbL3iaKZzUcmGls1teNKguo6VdErka',
+        'ADMIN',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP);
